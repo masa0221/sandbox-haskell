a30fa81db4063ca40e0191f04eab59719a94a3b3
masa0221 <0221masa@gmail.com>
2015-02-17 22:58:02 +0900

    first commit

---
+++ 1-baby.hs
@@ -0,0 +1,30 @@
+doubleMe x = x + x
+doubleUs x y = x * 2 + y * 2
+doubleSmallNumber x = if x > 100
+    then x
+    else x * 2
+doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
+conanO'Brien = "It's a-me, Conan O'Brien!"
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x ]
+length' xs = sum [1 | _ <- xs]
+
+removeNonUppercase :: [Char] -> [Char]
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
+
+factorial :: Integer -> Integer
+factorial n = product [1..n]
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
+
+strTest :: Char -> Char
+strTest str = str
+
+tapleTest :: (Int, Char) -> (Int, Char, Bool)
+tapleTest (i, c) = (i, c, True)
