10bf237850d62d0feb8b02c18aee93ea2f605d7a
masa0221 <0221masa@gmail.com>
2015-03-15 22:08:08 +0900

    add chapter3 example

--- 3-function.hs
+++ 3-function.hs
@@ -67,3 +67,83 @@ a `myCompare` b
     | a == b    = EQ
     | a <= b    = LT
     | otherwise = GT
+
+-- BMI
+bmiTell' :: Double -> Double -> String
+bmiTell' weight height
+    | bmi <= skinny = "You're underweight, you emo, you!"
+    | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"
+    | bmi <= fat    = "You're fat! Lose some weight, fatty!"
+    | otherwise     = "You're a whale, congratulations!"
+    where bmi = weight / height ^ 2
+          skinny = 18.5
+          normal = 25.0
+          fat    = 30.0
+          
+
+-- greet :: String -> String
+-- greet "Juan" = niceGreeting ++ " Juan!"
+-- greet "Fernando" = niceGreeting ++ " Fernando!"
+-- greet name = badGreeting ++ " " ++ name
+--     where niceGreeting = "Hello! So verry nice to see you."
+--           badGreeting  = "Oh! Pfft. It's you."
+
+niceGreeting :: String
+niceGreeting = "Hello! So verry nice to see you."
+badGreeting :: String
+badGreeting  = "Oh! Pfft. It's you."
+
+greet :: String -> String
+greet "Juan" = niceGreeting ++ " Juan!"
+greet "Fernando" = niceGreeting ++ " Fernando!"
+greet name = badGreeting ++ " " ++ name
+
+
+-- memo.
+-- "abc" and 'a':'b':'c':[] are equals.
+initials :: String -> String -> String
+initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
+    where (f:_) = firstname
+          (l:_) = lastname
+
+-- case using "where"
+calcBmis :: [(Double, Double)] -> [Double]
+calcBmis xs = [bmi w h | (w, h) <- xs]
+    where bmi weight height = weight / height ^ 2
+
+-- case using "let"
+calcBmis' :: [(Double, Double)] -> [Double]
+-- calcBmis' xs = [bmi | (w, h) <- xs, let bmi = weight / height ^ 2]
+calcBmis' xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2, bmi > 25.0]
+
+-- case using "let"
+cylinder :: Double -> Double -> Double
+cylinder r h = 
+    let sideArea = 2 * pi * r * h
+        topArea  = pi * r ^ 2
+    in  sideArea + 2 * topArea    
+
+-- case using "where"
+cylinder' :: Double -> Double -> Double
+cylinder' r h = sideArea + 2 * topArea
+    where sideArea = 2 * pi * r * h
+          topArea  = pi * r ^ 2
+
+-- head' :: [a] -> a
+-- head' [] = error "Can't call head on an empty list, dummy!"
+-- head' (x:_) = x
+head'' :: [a] -> a
+head'' xs = case xs of []    -> error "Can't call head on an empty list, dummy!"
+                       (x:_) -> x
+
+describeList :: [a] -> String
+describeList ls = "The list is "
+                  ++ case ls of []  -> "empty."
+                                [x] -> "a singleton list."
+                                xs  -> "a longer list."
+
+describeList' :: [a] -> String
+describeList' ls = "The list is " ++ what ls
+    where what []  = "empty."
+          what [x] = "a singleton list."
+          what xs  = "a longer list."
