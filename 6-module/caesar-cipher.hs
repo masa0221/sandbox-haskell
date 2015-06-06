6f0edaf998afd9e6199302615ecfdcbdd6d669a5
masa0221 <0221masa@gmail.com>
2015-06-06 23:10:46 +0900

    Add sample of chapter 6

---
+++ 6-module/caesar-cipher.hs
@@ -0,0 +1,8 @@
+import Data.Char
+
+encode :: Int -> String -> String
+-- encode offset msg = map (\c -> chr $ ord c + offset) msg
+encode offset msg = map (chr . (+ offset) . ord) msg
+
+decode :: Int -> String -> String
+decode shift msg = encode (negate shift) msg
